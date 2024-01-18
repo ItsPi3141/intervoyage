4494f8470cc5ffaa563c68b4b560e10ada4dc9d8
Pi <piisthebestthingintheuniverse@gmail.com>
2024-01-18 12:50:01 -0800

    fixed users not being able to delete their own generated images

--- buttonInteractions/delete.js
+++ buttonInteractions/delete.js
@@ -4,7 +4,7 @@ module.exports = {
 	data: new ButtonBuilder().setCustomId("delete").setLabel("🗑️").setStyle(ButtonStyle.Danger),
 	async execute(/** @type {import("discord.js").Interaction} */ interaction) {
 		await interaction.client.application.fetch();
-		if (![interaction.message.author.id, interaction.client.application.owner.id].includes(interaction.user.id)) {
+		if (![interaction.message.interaction.user.id, interaction.client.application.owner.id].includes(interaction.user.id)) {
 			return;
 		}
 		return interaction.message.delete();
